59cc4c6cbbbe0184457e1fdef4b38f2019577930
Olga Lazarova <olahla@gmail.com>
2012-02-27 16:38:14 +0100

    added placeholder, item click listener

--- Gallery Listing/src/com/chute/android/gcchutelisting/app/GalleryListingActivity.java
+++ Gallery Listing/src/com/chute/android/gcchutelisting/app/GalleryListingActivity.java
@@ -2,6 +2,9 @@ package com.chute.android.gcchutelisting.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,45 +18,62 @@ import com.chute.sdk.utils.GCConstants;
 
 public class GalleryListingActivity extends Activity {
 
-    @SuppressWarnings("unused")
-    private static final String TAG = GalleryListingActivity.class.getSimpleName();
-    private GalleryListingAdapter adapter;
-    public ListView listView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.chute_list);
-	listView = (ListView) findViewById(R.id.albumList);
-	GCUser.userChutes(getApplicationContext(), GCConstants.CURRENT_USER_ID,
-		new GCChuteCollectionCallback()).executeAsync();
-
-    }
-
-    private final class GCChuteCollectionCallback implements GCHttpCallback<GCChuteCollection> {
+	@SuppressWarnings("unused")
+	private static final String TAG = GalleryListingActivity.class
+			.getSimpleName();
+	private GalleryListingAdapter adapter;
+	public ListView listView;
 
 	@Override
-	public void onSuccess(GCChuteCollection responseData) {
-	    adapter = new GalleryListingAdapter(GalleryListingActivity.this, responseData);
-	    listView.setAdapter(adapter);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chute_list);
+		listView = (ListView) findViewById(R.id.albumList);
+		GCUser.userChutes(getApplicationContext(), GCConstants.CURRENT_USER_ID,
+				new GCChuteCollectionCallback()).executeAsync();
+
 	}
 
-	@Override
-	public void onHttpException(GCHttpRequestParameters params, Throwable exception) {
-	    Toast.makeText(getApplicationContext(), getString(R.string.http_exception),
-		    Toast.LENGTH_SHORT).show();
+	private final class GCChuteCollectionCallback implements
+			GCHttpCallback<GCChuteCollection> {
+
+		@Override
+		public void onSuccess(GCChuteCollection responseData) {
+			adapter = new GalleryListingAdapter(GalleryListingActivity.this,
+					responseData);
+			listView.setAdapter(adapter);
+			listView.setOnItemClickListener(new ListItemClickListener());
+		}
+
+		@Override
+		public void onHttpException(GCHttpRequestParameters params,
+				Throwable exception) {
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.http_exception), Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		@Override
+		public void onHttpError(int responseCode, String statusMessage) {
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.http_error), Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onParserException(int responseCode, Throwable exception) {
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.parsing_exception), Toast.LENGTH_SHORT)
+					.show();
+		}
 	}
 
-	@Override
-	public void onHttpError(int responseCode, String statusMessage) {
-	    Toast.makeText(getApplicationContext(), getString(R.string.http_error),
-		    Toast.LENGTH_SHORT).show();
-	}
+	private final class ListItemClickListener implements OnItemClickListener {
 
-	@Override
-	public void onParserException(int responseCode, Throwable exception) {
-	    Toast.makeText(getApplicationContext(), getString(R.string.parsing_exception),
-		    Toast.LENGTH_SHORT).show();
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			finish();
+		}
+
 	}
-    }
 }

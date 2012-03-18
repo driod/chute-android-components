4d15b1c3b8cdeabcc7d33eff10ddf4d6d1adec33
darko1002001 <darkogrozdanovski@gmail.com>
2012-03-18 22:43:12 +0100

    Added javascript true to webview

--- Image Sharer/src/com/chute/android/gcshareview/dialogs/BaseDialog.java
+++ Image Sharer/src/com/chute/android/gcshareview/dialogs/BaseDialog.java
@@ -13,56 +13,57 @@ import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 public class BaseDialog extends Dialog {
-	
-	 private ProgressBar pb;
 
-	public BaseDialog(Context context) {
-		super(context, android.R.style.Theme_Light);
+    private ProgressBar pb;
+
+    public BaseDialog(Context context) {
+	super(context, android.R.style.Theme_Light);
+    }
+
+    public static final String TAG = BaseDialog.class.getSimpleName();
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+	super.onCreate(savedInstanceState);
+	webView = new WebView(getContext());
+	webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+	webView.setWebViewClient(new BaseWebViewClient());
+	webView.getSettings().setJavaScriptEnabled(true);
+	FrameLayout frameLayout = new FrameLayout(getContext());
+	frameLayout.setMinimumHeight(150);
+	pb = new ProgressBar(getContext());
+	FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(100, 100);
+	layoutParams.gravity = Gravity.CENTER;
+	pb.setLayoutParams(layoutParams);
+	frameLayout.addView(webView);
+	frameLayout.addView(pb);
+	setContentView(frameLayout);
+    }
+
+    private class BaseWebViewClient extends WebViewClient {
+	@Override
+	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	    view.loadUrl(url);
+	    return true;
 	}
 
-	public static final String TAG = BaseDialog.class.getSimpleName();
-	private WebView webView;
-	
-	 @Override
-	    protected void onCreate(Bundle savedInstanceState) {
-		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		super.onCreate(savedInstanceState);
-		webView = new WebView(getContext());
-		webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-		webView.setWebViewClient(new BaseWebViewClient());
-		FrameLayout frameLayout = new FrameLayout(getContext());
-		frameLayout.setMinimumHeight(150);
-		pb = new ProgressBar(getContext());
-		FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(100, 100);
-		layoutParams.gravity = Gravity.CENTER;
-		pb.setLayoutParams(layoutParams);
-		frameLayout.addView(webView);
-		frameLayout.addView(pb);
-		setContentView(frameLayout);
-	    }
+	@Override
+	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+	    pb.setVisibility(View.VISIBLE);
+	    super.onPageStarted(view, url, favicon);
+	}
 
-	    private class BaseWebViewClient extends WebViewClient {
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		    view.loadUrl(url);
-		    return true;
-		}
+	@Override
+	public void onPageFinished(WebView view, String url) {
+	    pb.setVisibility(View.GONE);
+	    super.onPageFinished(view, url);
+	}
+    }
 
-		@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-		    pb.setVisibility(View.VISIBLE);
-		    super.onPageStarted(view, url, favicon);
-		}
+    public void loadUrl(String url) {
+	webView.loadUrl(url);
+    }
 
-		@Override
-		public void onPageFinished(WebView view, String url) {
-		    pb.setVisibility(View.GONE);
-		    super.onPageFinished(view, url);
-		}
-	    }
-	    
-	    public void loadUrl(String url) {
-	    	webView.loadUrl(url);
-	    }
-	    
 }
--- Social Gallery/src/com/chute/android/socialgallery/app/SocialGalleryApp.java
+++ Social Gallery/src/com/chute/android/socialgallery/app/SocialGalleryApp.java
@@ -3,14 +3,14 @@ package com.chute.android.socialgallery.app;
 import android.app.Application;
 import android.content.Context;
 
-import com.chute.android.gallery.R;
+import com.chute.android.socialgallery.R;
 import com.chute.sdk.utils.GCUtils;
 import com.darko.imagedownloader.ImageLoader;
 
 public class SocialGalleryApp extends Application {
 
-	public static final String TAG = SocialGalleryApp.class.getSimpleName();
-	
+    public static final String TAG = SocialGalleryApp.class.getSimpleName();
+
     private static ImageLoader createImageLoader(Context context) {
 	final ImageLoader imageLoader = new ImageLoader(context, R.drawable.placeholder_image_small);
 	imageLoader.setDefaultImageSize(GCUtils.pixelsFromDp(context, 75));
